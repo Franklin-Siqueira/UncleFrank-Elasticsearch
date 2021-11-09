ae5bc7bcccb9bf4766d21d92a12e107c179c40f8
Franklin-Siqueira <franklin.cs@outlook.com>
2021-11-09 14:08:30 -0300

    Initial commit

---
+++ Product-API-ES/src/main/java/siqueira/franklin/ProductAPIES/ProductApiEsApplication.java
@@ -0,0 +1,13 @@
+package siqueira.franklin.ProductAPIES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiEsApplication.class, args);
+	}
+
+}
---
+++ Product-API-ES/src/test/java/siqueira/franklin/ProductAPIES/ProductApiEsApplicationTests.java
@@ -0,0 +1,13 @@
+package siqueira.franklin.ProductAPIES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
